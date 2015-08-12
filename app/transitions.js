cf160236b1280ff63028a7d040c3caebf19365bc
Meghan <mmhade@gmail.com>
2015-08-12 12:47:05 -0700

    animate avatar through route change

--- app/transitions.js
+++ app/transitions.js
@@ -1,8 +1,16 @@
 export default function(){
-  this.transition(
-    this.fromRoute('user.index'),
-    this.toRoute('user.following'),
-    this.use('toLeft'),
-    this.reverse('toRight')
-  );
+	this.transition(
+	this.fromRoute('user.index'),
+	this.toRoute('user.following'),
+	this.use('toLeft'),
+	this.reverse('toRight')
+	);
+  	this.transition(
+	  this.fromRoute('home'),
+	  this.toRoute('user'),
+	  this.useAndReverse('explode', {
+	    matchBy: 'data-profile-image',
+	    use: 'fly-to'
+	  })
+	);
 }
