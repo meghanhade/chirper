12aade1ab5325a15e85c96def25c3f538745db81
Meghan <mmhade@gmail.com>
2015-08-12 11:52:09 -0700

    profile page complete

--- app/user/model.js
+++ app/user/model.js
@@ -1,10 +1,17 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 var User = DS.Model.extend({
   username: DS.attr('string'),
-  numberOfChirps: DS.attr('number'),
-  numberOfFollowing: DS.attr('number'),
-  numberOfFollowers: DS.attr('number'),
+  numberOfChirps: Ember.computed('chirps', function() {
+    return this.get('chirps').get('length');
+  }),
+  numberOfFollowing: Ember.computed('followees', function() {
+    return this.get('followees').get('length');
+  }),
+  numberOfFollowers: Ember.computed('followers', function() {
+    return this.get('followers').get('length');
+  }),
   aboutMe: DS.attr('string'),
   joinedAt: DS.attr('date'),
   chirps: DS.hasMany('chirps', { async: true }),
@@ -22,13 +29,10 @@ User.reopenClass({
   FIXTURES: [
     { 
       id: 1, 
-      username: 't4t5', // Put your username here
-      numberOfChirps: 2,
-      numberOfFollowing: 5,
-      numberOfFollowers: 5,
+      username: 'Meghan',
       aboutMe: 'I like making stuff.',
       joinedAt: new Date('2015-06-08T09:30:26'),
-      chirps: [1, 2],
+      chirps: [1, 2, 3],
       followees: [1],
       followers: [1]
     }
